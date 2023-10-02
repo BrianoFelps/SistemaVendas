ccf1d7a8db3b7e4841908afa9c24927023b5c7d2
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2023-10-02 17:00:27 -0300

    SistemaAdicionado

---
+++ src/main/java/com/mycompany/sistemavendas/SistemaVendas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemavendas;
+
+/**
+ *
+ * @author brian.7908
+ */
+public class SistemaVendas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
