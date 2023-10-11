a831624d7eea551eedd4563f74bf29b58decfbd3
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2023-10-11 14:26:30 -0300

    +ModEstCiv

--- src/main/java/com/mycompany/modelo/ModEstciv.java
+++ src/main/java/com/mycompany/modelo/ModEstciv.java
@@ -9,5 +9,34 @@ package com.mycompany.modelo;
  * @author brian.7908
  */
 public class ModEstciv {
-    
+    private int id;
+    private String nome;
+
+    public ModEstciv() {
+    }
+
+    public ModEstciv(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    @Override
+    public String toString(){
+        return "Estado Civil{" + "id=" + id + ", nome=" + nome + "}";
+    }
 }
